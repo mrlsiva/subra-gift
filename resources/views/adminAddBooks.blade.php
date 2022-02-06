3df86e28289c398446f47cf986561855a71c9d0a
csarvangit <csarvan.tech@gmail.com>
2022-02-06 22:08:12 +0530

    rename

--- resources/views/adminAddBooks.blade.php
+++ resources/views/adminAddBooks.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Add products</div>
+                <div class="card-header">Add product</div>
 
                 <div class="card-body">
                     @if(Session::has('message'))
